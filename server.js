de721c1398e69e701b8fb5e3ba7b2c19708bd7df
rongbang <rongbang.ye@gmail.com>
2020-10-04 11:52:21 -0700

    make it better

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
 // Instantiate the server, then tell it to listen for request
 const app = express();
 
+// API GET Requests
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    dbData = JSON.parse(data);
+    res.send(dbData);
+  });
+});
+
 // add a route. This can go under the existing routes but above app.listen()
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.listen(PORT, () => {
